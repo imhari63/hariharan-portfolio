b837b1c29c55bd201fc79e80b8c84e55fd77fa15
Hariharan R <rhariharan1204@gmail.com>
2025-11-11 14:48:29 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "Hariharan R",
   profile: '/images/profile.jpg',
-  designation: "Organoid Intelligence Researcher",
+  designation: "Free Lancer",
   description: "My name is Hariharan R. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'rhariharan1204@gmail.com',
   phone: '+919003960842',
