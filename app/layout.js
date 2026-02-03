049a41a78bb09eef6652515629950f72f882f0d8
Hariharan R <rhariharan1204@gmail.com>
2026-02-03 16:57:59 +0530

    Update metadataBase and add canonical URL
    Updated metadataBase and canonical URL for deployment.

--- app/layout.js
+++ app/layout.js
@@ -2,11 +2,14 @@ import { GoogleTagManager } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 import Footer from "./components/footer";
 import ScrollToTop from "./components/helper/scroll-to-top";
 import Navbar from "./components/navbar";
+
 import "./css/card.scss";
 import "./css/globals.scss";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -17,7 +20,10 @@ export const metadata = {
   description:
     "Hariharan R is a software developer and full-stack engineer. Explore projects, skills, and experience.",
   applicationName: "Hariharan R",
-  metadataBase: new URL("https://yourdomain.com"), // change if you have domain
+  metadataBase: new URL("https://your-vercel-url.vercel.app"), // replace when you buy domain
+  alternates: {
+    canonical: "https://your-vercel-url.vercel.app",
+  },
   icons: {
     icon: "/favicon.png",
   },
@@ -32,20 +38,22 @@ export const metadata = {
   },
 };
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
+
         <ToastContainer />
+
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
           {children}
           <ScrollToTop />
         </main>
+
         <Footer />
       </body>
-      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
     </html>
   );
 }
